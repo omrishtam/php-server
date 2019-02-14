0589c260ce50f8e0c9aa8f0ea432560d076e2000
Omri Shtamberger <omrishtam@gmail.com>
2019-02-14 21:58:10 +0200

    implement basic http server, add Dockerfile, docker-compose.yml
    Signed-off-by: Omri Shtamberger <omrishtam@gmail.com>

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"log"
+)
 
+const (
+	address string = ":8080"
+)
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	log.Printf("Server listening on %s", address)
+	http.ListenAndServe(address, nil)
 }
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		w.Write([]byte("Home Page"))
+	}
+} 
