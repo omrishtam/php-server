b453fc57092cc463671cd48f724280d7ac2f451d
Omri Shtamberger <omrishtam@gmail.com>
2019-02-14 22:31:57 +0200

    get server port and address from environment variable
    Signed-off-by: Omri Shtamberger <omrishtam@gmail.com>

--- main.go
+++ main.go
@@ -2,17 +2,47 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"strings"
 	"log"
 )
 
-const (
-	address string = ":8080"
-)
-
 func main() {
+	envMap := mapEnv(os.Environ())
+
 	http.HandleFunc("/", homeHandler)
-	log.Printf("Server listening on %s", address)
-	http.ListenAndServe(address, nil)
+	port := envMap["PORT"]
+	address := envMap["ADDRESS"]
+	if address == "" {
+		address = ":"
+	}
+
+	if port == "" {
+		port = "8080"
+	}
+
+	err := http.ListenAndServe(address + port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func mapEnv(env []string) map[string]string {
+	mapped := map[string]string{}
+
+	for _, val := range env {
+		sep := strings.Index(val, "=")
+
+		if sep > 0 {
+			key := val[0:sep]
+			value := val[sep+1:]
+			mapped[key] = value
+		} else {
+			log.Println("Bad environment: " + val)
+		}
+	}
+
+	return mapped
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
