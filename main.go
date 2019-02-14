66973bca943c77f2e000dbabad7abb4d3600c32b
Omri Shtamberger <omrishtam@gmail.com>
2019-02-14 21:57:01 +0200

    Revert "implement basic http server, add Dockerfile, docker-compose.yml"
    This reverts commit d515eb6d2b8e8a424346996cd24484002bce2e19.

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"net/http"
-	"log"
-)
-
-const (
-	address string = ":8080"
-)
-
 func main() {
-	http.HandleFunc("/", homeHandler)
-	log.Printf("Server listening on %s", address)
-	http.ListenAndServe(address, nil)
-}
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		w.Write([]byte("Home Page"))
-	}
-} 
+}
