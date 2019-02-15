4af315f6e8e83940ac131a67a7174a47a57dc363
Omri Shtamberger <omrishtam@gmail.com>
2019-02-15 14:44:34 +0200

    split long line
    Signed-off-by: Omri Shtamberger <omrishtam@gmail.com>

--- main.go
+++ main.go
@@ -13,7 +13,11 @@ import (
 
 func main() {
 	var wait time.Duration
-    flag.DurationVar(&wait, "graceful-timeout", time.Second * 15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+    flag.DurationVar(&wait,
+		"graceful-timeout",
+		time.Second * 15,
+		"the duration for which the server gracefully" +
+		"wait for existing connections to finish - e.g. 15s or 1m")
 	flag.Parse()
 	
 	address := os.Getenv("ADDRESS")
