be342aeb5014146fa5532628e741f6104338f70c
Omri Shtamberger <omrishtam@gmail.com>
2019-02-15 12:55:37 +0200

    add stub for GetUser route with mux
    Signed-off-by: Omri Shtamberger <omrishtam@gmail.com>

--- main.go
+++ main.go
@@ -1,48 +1,25 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"net/http"
 	"os"
-	"strings"
 	"log"
 )
 
 func main() {
-	envMap := mapEnv(os.Environ())
-
-	http.HandleFunc("/", homeHandler)
-	address := envMap["ADDRESS"]
-	port := envMap["PORT"]
+	address := os.Getenv("ADDRESS")
+	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-
+	
+	userHandler := UserHandler{}
+	r := mux.NewRouter()
+	r.HandleFunc("/user/{id}", userHandler.getUserHandler).Methods("GET")
+	http.Handle("/", r)
 	err := http.ListenAndServe(address + ":" + port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
-
-func mapEnv(env []string) map[string]string {
-	mapped := map[string]string{}
-
-	for _, val := range env {
-		sep := strings.Index(val, "=")
-
-		if sep > 0 {
-			key := val[0:sep]
-			value := val[sep+1:]
-			mapped[key] = value
-		} else {
-			log.Println("Bad environment: " + val)
-		}
-	}
-
-	return mapped
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		w.Write([]byte("Home Page"))
-	}
-} 
