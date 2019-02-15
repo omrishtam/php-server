54dad3fb824359a8f5953aedcca1e50e67ad4a05
Omer Zamir <zamir98@gmail.com>
2019-02-15 16:07:49 +0200

    Feature/mongo connection (#8)
    * start mongo
    
    Signed-off-by: Omer Zamir <zamir98@gmail.com>
    
    * merge conflicts resolved
    
    Signed-off-by: Omer Zamir <zamir98@gmail.com>
    
    * merge conflicts resolved
    
    Signed-off-by: Omer Zamir <zamir98@gmail.com>
    
    * merge conflicts resolved
    
    Signed-off-by: Omer Zamir <zamir98@gmail.com>
    
    * added db connection
    
    Signed-off-by: Omer Zamir <zamir98@gmail.com>
    
    * fixed identation and added env variables to docker compose.
    
    Signed-off-by: Omer Zamir <zamir98@gmail.com>

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	"net/http"
-	"os"
-	"os/signal"
+	"fmt"
 	"context"
 	"log"
 	"time"
 	"flag"
+	"net/http"
+	"os"
+	"os/signal"
+	"github.com/mongodb/mongo-go-driver/mongo"
+	"github.com/gorilla/mux"
+)
+
+type key string
+
+const (
+	hostKey     = key("hostKey")
+	databaseKey = key("databaseKey")
+	usernameKey = key("usernameKey")
+	passwordKey = key("passwordKey")
 )
 
 func main() {
@@ -24,6 +35,17 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
+	}
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	ctx = context.WithValue(ctx, hostKey, os.Getenv("MONGO_HOST"))
+	ctx = context.WithValue(ctx, usernameKey, os.Getenv("MONGO_USERNAME"))
+	ctx = context.WithValue(ctx, passwordKey, os.Getenv("MONGO_PASSWORD"))
+	ctx = context.WithValue(ctx, databaseKey, os.Getenv("MONGO_DATABASE"))
+	db, err := configDB(ctx)
+	if err != nil {
+		log.Fatalf("database configuration failed: %v", err)
 	}
 	
 	userHandler := UserHandler{}
@@ -58,14 +80,41 @@ func main() {
     <-c
 
     // Create a deadline to wait for.
-    ctx, cancel := context.WithTimeout(context.Background(), wait)
+    gracefulShutdownCtx, cancel := context.WithTimeout(context.Background(), wait)
     defer cancel()
     // Doesn't block if no connections, but will otherwise wait
     // until the timeout deadline.
-    server.Shutdown(ctx)
+    server.Shutdown(gracefulShutdownCtx)
     // Optionally, you could run srv.Shutdown in a goroutine and block on
     // <-ctx.Done() if your application should wait for other services
     // to finalize based on context cancellation.
     log.Println("shutting down")
     os.Exit(0)
+}
+
+func configDB(ctx context.Context) (*mongo.Database, error) {
+	var uri string
+	if ctx.Value(usernameKey) != nil && ctx.Value(passwordKey) != nil {
+		uri = fmt.Sprintf(`mongodb://%s:%s@%s/%s`,
+			ctx.Value(usernameKey),
+			ctx.Value(passwordKey),
+			ctx.Value(hostKey),
+			ctx.Value(databaseKey),
+		)
+	} else {
+		uri = fmt.Sprintf(`mongodb://%s/%s`,
+			ctx.Value(hostKey),
+			ctx.Value(databaseKey),
+		)
+	}
+	client, err := mongo.NewClient(uri)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't connect to mongo: %v", err)
+	}
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("mongo client couldn't connect with background context: %v", err)
+	}
+	phpDB := client.Database("php")
+	return phpDB, nil
 }
