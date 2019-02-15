b835c9408290b4617178129b6276eb7cf0025ecf
Omer Zamir <zamir98@gmail.com>
2019-02-15 14:27:48 +0200

    added db connection
    Signed-off-by: Omer Zamir <zamir98@gmail.com>

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ require (
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
 	github.com/gorilla/mux v1.7.0
-	github.com/mongodb/mongo-go-driver v0.3.0 // indirect
+	github.com/mongodb/mongo-go-driver v0.3.0
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
 	golang.org/x/crypto v0.0.0-20190211182817-74369b46fc67 // indirect
--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"net/http"
 	"os"
 	"log"
+	"context"
+	"github.com/mongodb/mongo-go-driver/mongo"
+	"fmt"
 )
+
+type key string
 
 const (
 	hostKey     = key("hostKey")
 	databaseKey = key("databaseKey")
+	usernameKey = key("usernameKey")
+	passwordKey = key("passwordKey")
 )
 
 func main() {
@@ -22,6 +29,8 @@ func main() {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 	ctx = context.WithValue(ctx, hostKey, os.Getenv("MONGO_HOST"))
+	ctx = context.WithValue(ctx, usernameKey, os.Getenv("MONGO_USERNAME"))
+	ctx = context.WithValue(ctx, passwordKey, os.Getenv("MONGO_PASSWORD"))
 	ctx = context.WithValue(ctx, databaseKey, os.Getenv("MONGO_DATABASE"))
 	db, err := configDB(ctx)
 	if err != nil {
@@ -32,8 +41,35 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/user/{id}", userHandler.GetUserHandler).Methods("GET")
 	http.Handle("/", r)	
-	err := http.ListenAndServe(address+":"+port, nil)
+	err = http.ListenAndServe(address+":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func configDB(ctx context.Context) (*mongo.Database, error) {
+	var uri string
+	if ctx.Value(usernameKey) != nil && ctx.Value(passwordKey) != nil {
+		uri = fmt.Sprintf(`mongodb://%s:%s@%s/%s`,
+		ctx.Value(usernameKey),
+		ctx.Value(passwordKey),
+		ctx.Value(hostKey),
+		ctx.Value(databaseKey),
+		)
+	} else {
+		uri = fmt.Sprintf(`mongodb://%s/%s`,
+		ctx.Value(hostKey),
+		ctx.Value(databaseKey),
+		)
+	}
+	client, err := mongo.NewClient(uri)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't connect to mongo: %v", err)
+	}
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("mongo client couldn't connect with background context: %v", err)
+	}
+	phpDB := client.Database("php")
+	return phpDB, nil
 }
